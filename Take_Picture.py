a1227bdc4eca954fcb6e494e24343e89e0414f95
Moez Abid <moez.abid.pro@gmail.com>
2020-02-03 22:36:19 +0100

    Original Resolution

--- Take_Picture.py
+++ Take_Picture.py
@@ -2,10 +2,13 @@
 import picamera
 import datetime
 
-print("Taking picture...")
-with picamera.PiCamera() as camera:
-    camera.resolution = (1280,720)
-    current_time = datetime.datetime.now()
-    image_name = "/home/pi/Pictures/" + str(current_time) + ".jpg"
-    camera.capture(image_name)
-print("Picture taken.")
+def take_picture():
+    print("Taking picture...")
+    with picamera.PiCamera() as camera:
+        #camera.resolution = (1280,720)
+        current_time = datetime.datetime.now()
+        image_name = "/home/pi/Pictures/" + str(current_time) + ".jpg"
+        camera.capture(image_name)
+    print("Picture taken.")
+
+take_picture()
